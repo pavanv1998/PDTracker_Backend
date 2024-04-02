6ff1d88bea468cad48370e6d0f78a0c1b693844c
Abhijith Chandra Mergu <abhijith.mergu@gmail.com>
2024-04-02 10:59:56 -0400

    Handled update landmarks

--- app/leg_raise_2.py
+++ app/leg_raise_2.py
@@ -14,7 +14,7 @@ from super_gradients.training import models
 
 from app.analysis.finderPeaksSignal import peakFinder
 
-from app.analysis.analysis import analysis
+from app.analysis.analysis import analysis, get_analysis_output
 # %%
 def filterSignal(rawSignal, fs=25, cutOffFrequency=5):
     b, a = signal.butter(2, cutOffFrequency, fs=fs, btype='low', analog=False)
@@ -578,226 +578,227 @@ def toe_tap_analysis(fps, bounding_box, start_time, end_time, input_video, is_le
     return jsonFinal
 
 def updateLandMarks(inputJson):
-
-    knee_landmarks = inputJson['landmarks']
+    task_name = inputJson['task_name']
+    display_landmarks = inputJson['landmarks']
     fps = inputJson['fps']
     start_time =  inputJson['start_time'] 
     end_time =  inputJson['end_time']
-    normalization_landmarks = inputJson['normalization_landmarks']
     normalization_factor = 1
     if 'normalization_factor' in inputJson:
         normalization_factor = inputJson['normalization_factor']
 
-    landmarks_signal = []
+    return get_analysis_output(task_name, display_landmarks, normalization_factor, fps, start_time, end_time)
 
-    for i in range(len(knee_landmarks)):
-        p = [knee_landmarks[i][0],knee_landmarks[i][1]]
-        q = [normalization_landmarks[i][0],normalization_landmarks[i][1]]
-        
-        landmarks_signal.append([0,(math.dist(p, q)/normalization_factor)])
-        #knee_landmarks_signal.append([knee_landmarks[i][0] - normalization_landmarks[i][0], knee_landmarks[i][1] - normalization_landmarks[i][1]])
-
-    signalOfInterest = np.array(landmarks_signal)[:, 1]
-    # signalOfInterest = np.array(knee_landmarks_signal)
-    # signalOfInterest = filterSignal(signalOfInterest, cutOffFrequency=7.5)
-
-    # find peaks using custom algorithm
-    currentFs = 1 / fps
-    desiredFs = 1 / 60
-
-    duration = end_time - start_time
-    print(duration)
-
-    timeVector = np.linspace(0, duration, int(duration / currentFs))
-
-    newTimeVector = np.linspace(0, duration, int(duration / desiredFs))
-    upsampleSignal = signal.resample(signalOfInterest, len(newTimeVector))
-
-    distance, velocity, peaks, indexPositiveVelocity, indexNegativeVelocity = peakFinder(upsampleSignal, fs=60,
-                                                                                         minDistance=3,
-                                                                                         cutOffFrequency=7.5, prct=0.05)
-    
-    #distance = signalOfInterest
-    # plot
-    # a plot like this should appear on the page.
-
-    # green dots -> Peaks
-    # red dots -> Left Valleys
-    # blue dots -> Right Valleys
-    # figure, ax = plt.subplots(figsize=(15, 5))
-    # ax.plot(range(len(distance)), distance)
-    # ax.set_ylim([np.min(distance), np.max(distance)])
-    # ax.set_xlim([0, len(distance)])
-    print(duration)
-    line_time = []
-    sizeOfDist = len(distance)
-    for index, item in enumerate(distance):
-        line_time.append((index / sizeOfDist) * duration + start_time)
-
-    line_peaks = []
-    line_peaks_time = []
-    line_valleys_start = []
-    line_valleys_start_time = []
-    line_valleys_end = []
-    line_valleys_end_time = []
-
-    line_valleys = []
-    line_valleys_time = []
-
-    for index, item in enumerate(peaks):
-        # ax.plot(item['openingValleyIndex'], distance[item['openingValleyIndex']], 'ro', alpha=0.75)
-        # ax.plot(item['peakIndex'], distance[item['peakIndex']], 'go', alpha=0.75)
-        # ax.plot(item['closingValleyIndex'], distance[item['closingValleyIndex']], 'bo', alpha=0.75)
-        # line_valleys.append(prevValley+item['openingValleyIndex'])
-
-        line_peaks.append(distance[item['peakIndex']])
-        line_peaks_time.append((item['peakIndex'] / sizeOfDist) * duration + start_time)
-
-        line_valleys_start.append(distance[item['openingValleyIndex']])
-        line_valleys_start_time.append((item['openingValleyIndex'] / sizeOfDist) * duration + start_time)
-
-        line_valleys_end.append(distance[item['closingValleyIndex']])
-        line_valleys_end_time.append((item['closingValleyIndex'] / sizeOfDist) * duration + start_time)
-
-        line_valleys.append(distance[item['openingValleyIndex']])
-        line_valleys_time.append((item['openingValleyIndex'] / sizeOfDist) * duration + start_time)
-
-    amplitude = []
-    peakTime = []
-    rmsVelocity = []
-    maxOpeningSpeed = []
-    maxClosingSpeed = []
-    averageOpeningSpeed = []
-    averageClosingSpeed = []
-
-    for idx, peak in enumerate(peaks):
-        # Height measures
-        x1 = peak['openingValleyIndex']
-        y1 = distance[peak['openingValleyIndex']]
-
-        x2 = peak['closingValleyIndex']
-        y2 = distance[peak['closingValleyIndex']]
-
-        x = peak['peakIndex']
-        y = distance[peak['peakIndex']]
-
-        f = interpolate.interp1d(np.array([x1, x2]), np.array([y1, y2]))
-
-        amplitude.append(y - f(x))
-
-        # Opening Velocity
-        rmsVelocity.append(np.sqrt(np.mean(velocity[peak['openingValleyIndex']:peak['closingValleyIndex']] ** 2)))
-
-        averageOpeningSpeed.append((y - f(x)) / ((peak['peakIndex'] - peak['openingValleyIndex']) * (1 / 60)))
-        averageClosingSpeed.append((y - f(x)) / ((peak['closingValleyIndex'] - peak['peakIndex']) * (1 / 60)))
-
-        # timming
-        peakTime.append(peak['peakIndex'] * (1 / 60))
-
-    meanAmplitude = np.mean(amplitude)
-    stdAmplitude = np.std(amplitude)
-
-    meanRMSVelocity = np.mean(rmsVelocity)
-    stdRMSVelocity = np.std(rmsVelocity)
-    meanAverageOpeningSpeed = np.mean(averageOpeningSpeed)
-    stdAverageOpeningSpeed = np.std(averageOpeningSpeed)
-    meanAverageClosingSpeed = np.mean(averageClosingSpeed)
-    stdAverageClosingSpeed = np.std(averageClosingSpeed)
-
-    meanCycleDuration = np.mean(np.diff(peakTime))
-    stdCycleDuration = np.std(np.diff(peakTime))
-    rangeCycleDuration = np.max(np.diff(peakTime)) - np.min(np.diff(peakTime))
-    rate = len(peaks) / (peaks[-1]['closingValleyIndex'] - peaks[0]['openingValleyIndex']) / (1 / 60)
-
-    earlyPeaks = peaks[:len(peaks) // 3]
-    latePeaks = peaks[-len(peaks) // 3:]
-    amplitudeDecay = np.mean(distance[:len(peaks) // 3]) / np.mean(distance[-len(peaks) // 3:])
-    velocityDecay = np.sqrt(
-        np.mean(velocity[earlyPeaks[0]['openingValleyIndex']:earlyPeaks[-1]['closingValleyIndex']] ** 2)) / np.sqrt(
-        np.mean(velocity[latePeaks[0]['openingValleyIndex']:latePeaks[-1]['closingValleyIndex']] ** 2))
-    rateDecay = (len(earlyPeaks) / (
-            (earlyPeaks[-1]['closingValleyIndex'] - earlyPeaks[0]['openingValleyIndex']) / (1 / 60))) / (
-                        len(latePeaks) / (
-                        (latePeaks[-1]['closingValleyIndex'] - latePeaks[0]['openingValleyIndex']) / (1 / 60)))
-
-    cvAmplitude = stdAmplitude / meanAmplitude
-    cvCycleDuration = stdCycleDuration / meanCycleDuration
-    cvRMSVelocity = stdRMSVelocity / meanRMSVelocity
-    cvAverageOpeningSpeed = stdAverageOpeningSpeed / meanAverageOpeningSpeed
-    cvAverageClosingSpeed = stdAverageClosingSpeed / meanAverageClosingSpeed
-
-    # results = np.array([meanAmplitude, stdAmplitude,
-    #                     meanRMSVelocity, stdRMSVelocity, meanAverageOpeningSpeed, stdAverageOpeningSpeed,
-    #                     meanAverageClosingSpeed, stdAverageClosingSpeed,
-    #                     meanCycleDuration, stdCycleDuration, rangeCycleDuration, rate,
-    #                     amplitudeDecay, velocityDecay, rateDecay,
-    #                     cvAmplitude, cvCycleDuration, cvRMSVelocity, cvAverageOpeningSpeed, cvAverageClosingSpeed])
-
-    jsonFinal = {
-        "linePlot": {
-            "data": distance,
-            "time": line_time
-        },
-        "peaks": {
-            "data": line_peaks,
-            "time": line_peaks_time
-        },
-        "valleys": {
-            "data": line_valleys,
-            "time": line_valleys_time
-        },
-        "valleys_start": {
-            "data": line_valleys_start,
-            "time": line_valleys_start_time
-        },
-        "valleys_end": {
-            "data": line_valleys_end,
-            "time": line_valleys_end_time
-        },
-        "radar": {
-            "A": [2.5, 3.8, 5.9, 4.2, 8.6, 4.9, 6.9, 9, 8.4, 7.3],
-            "B": [3.8, 7.1, 7.4, 5.9, 4.2, 3.1, 6.5, 6.4, 3, 7],
-            "labels": ["Mean Frequency", "Mean cycle amplitude", "CV cycle amplitude", "Mean cycle rms velocity",
-                       "CV cycle rms velocity",
-                       "Mean cycle duration", "CV cycle duration", "Range cycle duration", "Amplitude decay",
-                       "Velocity decay"],
-            "velocity": velocity
-        },
-        "radarTable": {
-            "MeanAmplitude": meanAmplitude,
-            "StdAmplitude": stdAmplitude,
-            "MeanRMSVelocity": meanRMSVelocity,
-            "StdRMSVelocity": stdRMSVelocity,
-            "meanAverageOpeningSpeed": meanAverageOpeningSpeed,
-            "stdAverageOpeningSpeed": stdAverageOpeningSpeed,
-            "meanAverageClosingSpeed": meanAverageClosingSpeed,
-            "stdAverageClosingSpeed": stdAverageClosingSpeed,
-            "meanCycleDuration": meanCycleDuration,
-            "stdCycleDuration": stdCycleDuration,
-            "rangeCycleDuration": rangeCycleDuration,
-            "rate": rate,
-            "amplitudeDecay": amplitudeDecay,
-            "velocityDecay": velocityDecay,
-            "rateDecay": rateDecay,
-            "cvAmplitude": cvAmplitude,
-            "cvCycleDuration": cvCycleDuration,
-            "cvRMSVelocity": cvRMSVelocity,
-            "cvAverageOpeningSpeed": cvAverageOpeningSpeed,
-            "cvAverageClosingSpeed": cvAverageClosingSpeed
-        },
-        "landMarks": knee_landmarks,
-        "normalization_landmarks": normalization_landmarks,
-        "normalization_factor": normalization_factor
-
-    }
-
-    json_object = json.dumps(jsonFinal, default=json_serialize)
- 
-# Writing to sample.json
-    with open("sample3.json", "w") as outfile:
-        outfile.write(json_object)
-
-    return jsonFinal
+#     landmarks_signal = []
+#
+#     for i in range(len(display_landmarks)):
+#         p = [display_landmarks[i][0],display_landmarks[i][1]]
+#         q = [normalization_landmarks[i][0],normalization_landmarks[i][1]]
+#
+#         landmarks_signal.append([0,(math.dist(p, q)/normalization_factor)])
+#         #knee_landmarks_signal.append([display_landmarks[i][0] - normalization_landmarks[i][0], display_landmarks[i][1] - normalization_landmarks[i][1]])
+#
+#     signalOfInterest = np.array(landmarks_signal)[:, 1]
+#     # signalOfInterest = np.array(knee_landmarks_signal)
+#     # signalOfInterest = filterSignal(signalOfInterest, cutOffFrequency=7.5)
+#
+#     # find peaks using custom algorithm
+#     currentFs = 1 / fps
+#     desiredFs = 1 / 60
+#
+#     duration = end_time - start_time
+#     print(duration)
+#
+#     timeVector = np.linspace(0, duration, int(duration / currentFs))
+#
+#     newTimeVector = np.linspace(0, duration, int(duration / desiredFs))
+#     upsampleSignal = signal.resample(signalOfInterest, len(newTimeVector))
+#
+#     distance, velocity, peaks, indexPositiveVelocity, indexNegativeVelocity = peakFinder(upsampleSignal, fs=60,
+#                                                                                          minDistance=3,
+#                                                                                          cutOffFrequency=7.5, prct=0.05)
+#
+#     #distance = signalOfInterest
+#     # plot
+#     # a plot like this should appear on the page.
+#
+#     # green dots -> Peaks
+#     # red dots -> Left Valleys
+#     # blue dots -> Right Valleys
+#     # figure, ax = plt.subplots(figsize=(15, 5))
+#     # ax.plot(range(len(distance)), distance)
+#     # ax.set_ylim([np.min(distance), np.max(distance)])
+#     # ax.set_xlim([0, len(distance)])
+#     print(duration)
+#     line_time = []
+#     sizeOfDist = len(distance)
+#     for index, item in enumerate(distance):
+#         line_time.append((index / sizeOfDist) * duration + start_time)
+#
+#     line_peaks = []
+#     line_peaks_time = []
+#     line_valleys_start = []
+#     line_valleys_start_time = []
+#     line_valleys_end = []
+#     line_valleys_end_time = []
+#
+#     line_valleys = []
+#     line_valleys_time = []
+#
+#     for index, item in enumerate(peaks):
+#         # ax.plot(item['openingValleyIndex'], distance[item['openingValleyIndex']], 'ro', alpha=0.75)
+#         # ax.plot(item['peakIndex'], distance[item['peakIndex']], 'go', alpha=0.75)
+#         # ax.plot(item['closingValleyIndex'], distance[item['closingValleyIndex']], 'bo', alpha=0.75)
+#         # line_valleys.append(prevValley+item['openingValleyIndex'])
+#
+#         line_peaks.append(distance[item['peakIndex']])
+#         line_peaks_time.append((item['peakIndex'] / sizeOfDist) * duration + start_time)
+#
+#         line_valleys_start.append(distance[item['openingValleyIndex']])
+#         line_valleys_start_time.append((item['openingValleyIndex'] / sizeOfDist) * duration + start_time)
+#
+#         line_valleys_end.append(distance[item['closingValleyIndex']])
+#         line_valleys_end_time.append((item['closingValleyIndex'] / sizeOfDist) * duration + start_time)
+#
+#         line_valleys.append(distance[item['openingValleyIndex']])
+#         line_valleys_time.append((item['openingValleyIndex'] / sizeOfDist) * duration + start_time)
+#
+#     amplitude = []
+#     peakTime = []
+#     rmsVelocity = []
+#     maxOpeningSpeed = []
+#     maxClosingSpeed = []
+#     averageOpeningSpeed = []
+#     averageClosingSpeed = []
+#
+#     for idx, peak in enumerate(peaks):
+#         # Height measures
+#         x1 = peak['openingValleyIndex']
+#         y1 = distance[peak['openingValleyIndex']]
+#
+#         x2 = peak['closingValleyIndex']
+#         y2 = distance[peak['closingValleyIndex']]
+#
+#         x = peak['peakIndex']
+#         y = distance[peak['peakIndex']]
+#
+#         f = interpolate.interp1d(np.array([x1, x2]), np.array([y1, y2]))
+#
+#         amplitude.append(y - f(x))
+#
+#         # Opening Velocity
+#         rmsVelocity.append(np.sqrt(np.mean(velocity[peak['openingValleyIndex']:peak['closingValleyIndex']] ** 2)))
+#
+#         averageOpeningSpeed.append((y - f(x)) / ((peak['peakIndex'] - peak['openingValleyIndex']) * (1 / 60)))
+#         averageClosingSpeed.append((y - f(x)) / ((peak['closingValleyIndex'] - peak['peakIndex']) * (1 / 60)))
+#
+#         # timming
+#         peakTime.append(peak['peakIndex'] * (1 / 60))
+#
+#     meanAmplitude = np.mean(amplitude)
+#     stdAmplitude = np.std(amplitude)
+#
+#     meanRMSVelocity = np.mean(rmsVelocity)
+#     stdRMSVelocity = np.std(rmsVelocity)
+#     meanAverageOpeningSpeed = np.mean(averageOpeningSpeed)
+#     stdAverageOpeningSpeed = np.std(averageOpeningSpeed)
+#     meanAverageClosingSpeed = np.mean(averageClosingSpeed)
+#     stdAverageClosingSpeed = np.std(averageClosingSpeed)
+#
+#     meanCycleDuration = np.mean(np.diff(peakTime))
+#     stdCycleDuration = np.std(np.diff(peakTime))
+#     rangeCycleDuration = np.max(np.diff(peakTime)) - np.min(np.diff(peakTime))
+#     rate = len(peaks) / (peaks[-1]['closingValleyIndex'] - peaks[0]['openingValleyIndex']) / (1 / 60)
+#
+#     earlyPeaks = peaks[:len(peaks) // 3]
+#     latePeaks = peaks[-len(peaks) // 3:]
+#     amplitudeDecay = np.mean(distance[:len(peaks) // 3]) / np.mean(distance[-len(peaks) // 3:])
+#     velocityDecay = np.sqrt(
+#         np.mean(velocity[earlyPeaks[0]['openingValleyIndex']:earlyPeaks[-1]['closingValleyIndex']] ** 2)) / np.sqrt(
+#         np.mean(velocity[latePeaks[0]['openingValleyIndex']:latePeaks[-1]['closingValleyIndex']] ** 2))
+#     rateDecay = (len(earlyPeaks) / (
+#             (earlyPeaks[-1]['closingValleyIndex'] - earlyPeaks[0]['openingValleyIndex']) / (1 / 60))) / (
+#                         len(latePeaks) / (
+#                         (latePeaks[-1]['closingValleyIndex'] - latePeaks[0]['openingValleyIndex']) / (1 / 60)))
+#
+#     cvAmplitude = stdAmplitude / meanAmplitude
+#     cvCycleDuration = stdCycleDuration / meanCycleDuration
+#     cvRMSVelocity = stdRMSVelocity / meanRMSVelocity
+#     cvAverageOpeningSpeed = stdAverageOpeningSpeed / meanAverageOpeningSpeed
+#     cvAverageClosingSpeed = stdAverageClosingSpeed / meanAverageClosingSpeed
+#
+#     # results = np.array([meanAmplitude, stdAmplitude,
+#     #                     meanRMSVelocity, stdRMSVelocity, meanAverageOpeningSpeed, stdAverageOpeningSpeed,
+#     #                     meanAverageClosingSpeed, stdAverageClosingSpeed,
+#     #                     meanCycleDuration, stdCycleDuration, rangeCycleDuration, rate,
+#     #                     amplitudeDecay, velocityDecay, rateDecay,
+#     #                     cvAmplitude, cvCycleDuration, cvRMSVelocity, cvAverageOpeningSpeed, cvAverageClosingSpeed])
+#
+#     jsonFinal = {
+#         "linePlot": {
+#             "data": distance,
+#             "time": line_time
+#         },
+#         "peaks": {
+#             "data": line_peaks,
+#             "time": line_peaks_time
+#         },
+#         "valleys": {
+#             "data": line_valleys,
+#             "time": line_valleys_time
+#         },
+#         "valleys_start": {
+#             "data": line_valleys_start,
+#             "time": line_valleys_start_time
+#         },
+#         "valleys_end": {
+#             "data": line_valleys_end,
+#             "time": line_valleys_end_time
+#         },
+#         "radar": {
+#             "A": [2.5, 3.8, 5.9, 4.2, 8.6, 4.9, 6.9, 9, 8.4, 7.3],
+#             "B": [3.8, 7.1, 7.4, 5.9, 4.2, 3.1, 6.5, 6.4, 3, 7],
+#             "labels": ["Mean Frequency", "Mean cycle amplitude", "CV cycle amplitude", "Mean cycle rms velocity",
+#                        "CV cycle rms velocity",
+#                        "Mean cycle duration", "CV cycle duration", "Range cycle duration", "Amplitude decay",
+#                        "Velocity decay"],
+#             "velocity": velocity
+#         },
+#         "radarTable": {
+#             "MeanAmplitude": meanAmplitude,
+#             "StdAmplitude": stdAmplitude,
+#             "MeanRMSVelocity": meanRMSVelocity,
+#             "StdRMSVelocity": stdRMSVelocity,
+#             "meanAverageOpeningSpeed": meanAverageOpeningSpeed,
+#             "stdAverageOpeningSpeed": stdAverageOpeningSpeed,
+#             "meanAverageClosingSpeed": meanAverageClosingSpeed,
+#             "stdAverageClosingSpeed": stdAverageClosingSpeed,
+#             "meanCycleDuration": meanCycleDuration,
+#             "stdCycleDuration": stdCycleDuration,
+#             "rangeCycleDuration": rangeCycleDuration,
+#             "rate": rate,
+#             "amplitudeDecay": amplitudeDecay,
+#             "velocityDecay": velocityDecay,
+#             "rateDecay": rateDecay,
+#             "cvAmplitude": cvAmplitude,
+#             "cvCycleDuration": cvCycleDuration,
+#             "cvRMSVelocity": cvRMSVelocity,
+#             "cvAverageOpeningSpeed": cvAverageOpeningSpeed,
+#             "cvAverageClosingSpeed": cvAverageClosingSpeed
+#         },
+#         "landMarks": display_landmarks,
+#         "normalization_landmarks": normalization_landmarks,
+#         "normalization_factor": normalization_factor
+#
+#     }
+#
+#     json_object = json.dumps(jsonFinal, default=json_serialize)
+#
+# # Writing to sample.json
+#     with open("sample3.json", "w") as outfile:
+#         outfile.write(json_object)
+#
+#     return jsonFinal
     
 
 def updatePeaksAndValleys(inputJson):
