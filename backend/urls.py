b13f394b91909353b8f8b7eb3e1fdac19a1db40f
Abhijith Chandra Mergu <abhijith.mergu@gmail.com>
2024-04-22 02:30:43 -0400

    Added frontend

--- backend/urls.py
+++ backend/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from backend.views import home
 
 urlpatterns = [
+    path('', home),
     path('api/', include('app.urls')),
     path('admin/', admin.site.urls),
 ]
